baf7a5c6bb7bc1734021f53df79bdc4b716020a4
Jason Garber <jason@sixtwothree.org>
2020-01-23 20:47:46 -0500

    Poke around bookmarks_controller

--- app/rpc/bookmarks_controller.rb
+++ app/rpc/bookmarks_controller.rb
@@ -2,25 +2,22 @@ class BookmarksController < Gruf::Controllers::Base
   bind Rpc::Bookmarks::Service
 
   def get_bookmark
-    bookmark.to_proto
-  rescue StandardError
-    fail! :not_found, :bookmark_not_found, "Couldn't find Bookmark with 'id'=#{request.message.id}"
+    id = request.message.id
+
+    Bookmark.find(id).to_proto
+  rescue ActiveRecord::RecordNotFound => exception
+    fail! :not_found, :bookmark_not_found, exception.message
+  rescue StandardError => exception
+    fail! :internal, :unknown, exception.message
   end
 
   def list_bookmarks
     message = request.message
     limit = message.limit.positive? ? message.limit : 100
+    offset = message.offset.positive? ? message.offset : 0
 
-    bookmarks.limit(limit).offset(message.offset).map(&:to_proto)
-  end
-
-  private
-
-  def bookmark
-    @bookmark ||= Bookmark.find(request.message.id)
-  end
-
-  def bookmarks
-    @bookmarks ||= Bookmark.all
+    Bookmark.limit(limit).offset(offset).map(&:to_proto)
+  rescue StandardError => exception
+    fail! :internal, :unknown, exception.message
   end
 end
